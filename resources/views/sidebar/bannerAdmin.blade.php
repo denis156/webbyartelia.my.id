1e84caeb086a24752959775da1a7f86701585520
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-02-01 02:31:03 +0800

    Ubah nama kelas dan perbarui teks pada kartu dukungan admin untuk konsistensi dan pengalaman pengguna yang lebih baik

--- resources/views/sidebar/bannerAdmin.blade.php
+++ resources/views/sidebar/bannerAdmin.blade.php
@@ -1,13 +1,13 @@
-<div class="support-card-wrapper">
-    <div class="support-card">
+<div class="support-admin-wrapper">
+    <div class="support-admin-card">
         <div class="card-text">
             <span class="block text-white/80 text-sm mb-1">Butuh bantuan teknis?</span>
-            Konsultasi dengan Tim Support
+            Support Developer Artelia
         </div>
-        <a href="https://wa.me/6281524089375" target="_blank" class="support-button">
+        <a href="https://wa.me/6282141517722?text=Halo%20Developer%20Artelia%2C%20saya%20butuh%20bantuan%20teknis" target="_blank" class="support-button">
             <div class="flex items-center justify-center space-x-2">
-                @svg('heroicon-o-chat-bubble-left-right', 'w-5 h-5')
-                <span>Hubungi Support</span>
+                @svg('heroicon-o-code-bracket-square', 'w-5 h-5')
+                <span>Hubungi Developer</span>
             </div>
         </a>
     </div>
