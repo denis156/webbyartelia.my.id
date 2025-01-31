1fcfc81a0f7ae47421e4e4a385b6962b7b7168c2
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-02-01 02:38:02 +0800

    Perbarui nomor kontak pada banner dukungan admin dan klien untuk komunikasi yang lebih baik

--- resources/views/sidebar/bannerAdmin.blade.php
+++ resources/views/sidebar/bannerAdmin.blade.php
@@ -4,7 +4,7 @@
             <span class="block text-white/80 text-sm mb-1">Butuh bantuan teknis?</span>
             Support Developer Artelia
         </div>
-        <a href="https://wa.me/6282141517722?text=Halo%20Developer%20Artelia%2C%20saya%20butuh%20bantuan%20teknis" target="_blank" class="support-button">
+        <a href="https://wa.me/6281524089375?text=Halo%20Developer%20Artelia%2C%20saya%20butuh%20bantuan%20teknis" target="_blank" class="support-button">
             <div class="flex items-center justify-center space-x-2">
                 @svg('heroicon-o-code-bracket-square', 'w-5 h-5')
                 <span>Hubungi Developer</span>
--- resources/views/sidebar/bannerClient.blade.php
+++ resources/views/sidebar/bannerClient.blade.php
@@ -4,7 +4,7 @@
             <span class="block text-white/80 text-sm mb-1">Butuh bantuan project?</span>
             Admin Artelia siap membantu
         </div>
-        <a href="https://wa.me/6282141517722?text=Halo%20Admin%20Artelia%2C%20saya%20ingin%20konsultasi%20project" target="_blank" class="support-button">
+        <a href="https://wa.me/6281943462730?text=Halo%20Admin%20Artelia%2C%20saya%20ingin%20konsultasi%20project" target="_blank" class="support-button">
             <div class="flex items-center justify-center space-x-2">
                 @svg('heroicon-o-chat-bubble-left-right', 'w-5 h-5')
                 <span>Chat Admin</span>
