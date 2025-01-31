742f77909c88779c3280e99c76fe2761e79deaf9
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-02-01 02:17:23 +0800

    Tambahkan konfigurasi Tailwind CSS dan komponen card support untuk panel admin

---
+++ resources/views/sidebar/bannerAdmin.blade.php
@@ -0,0 +1,14 @@
+<div class="support-card-wrapper">
+    <div class="support-card">
+        <div class="card-text">
+            <span class="block text-white/80 text-sm mb-1">Butuh bantuan teknis?</span>
+            Konsultasi dengan Tim Support
+        </div>
+        <a href="https://wa.me/6281524089375" target="_blank" class="support-button">
+            <div class="flex items-center justify-center space-x-2">
+                @svg('heroicon-o-chat-bubble-left-right', 'w-5 h-5')
+                <span>Hubungi Support</span>
+            </div>
+        </a>
+    </div>
+</div>
