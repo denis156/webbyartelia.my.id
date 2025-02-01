f584b008ac0fbe2d5d8ef899993fe3d9ea3e8675
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-02-02 03:37:52 +0800

    Tambahkan opsi sidebarFullyCollapsibleOnDesktop pada panel admin Filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,6 +46,7 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => Color::Amber,
                 'secondary' => Color::Gray,
             ])
+            ->sidebarFullyCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
